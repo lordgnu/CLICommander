8707ac16533a7d33bb5a2ce97ddcf2d1fa06f699
Don Bauer <lordgnu@me.com>
2012-01-13 08:15:32 -0600

    Updated template example

--- examples/example.templateOutput.php
+++ examples/example.templateOutput.php
@@ -13,14 +13,18 @@ style the text in the template inline.
 
 To do this, simply use the following syntax: {{{red}FG_COLOR{reset}|{default|red}BG_COLOR{reset}|{default|default|bold}STYLE{reset}}}
 
-To display the curly brackets instead of attempting to use the tag, simply do two of them in a row {{
+To display the curly brackets instead of attempting to use the tag, 
+simply do two of them in a row {{
 
 It is important to note that color changes are persistant.
 
-So if I change the {|red}background to red then change the {cyan}foreground to cyan, you'll notice that the background is still red{reset}
-To reset the colors in a template, simply put a reset tag where you would like to change everything back to default.
+So if I change the {|red}background to red then change the {cyan}foreground to cyan, you'll 
+notice that the background is still red{reset}
+To reset the colors in a template, simply put a reset tag where you would like
+to change everything back to default.
 
-You can leave options you don't wish to change blank.  Please see the examples below:
+You can leave options you don't wish to change blank.  
+Please see the examples below:
 
 {blue}Just the foreground to blue{reset}
 {|blue}Just the background to blue{reset}
